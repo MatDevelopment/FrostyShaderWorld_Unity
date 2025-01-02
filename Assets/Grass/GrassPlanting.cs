c40bb2fea86fb4109273a526196d018af37f9f29
marchusl <83002021+marchusl@users.noreply.github.com>
2025-01-02 22:49:18 +0100

    Realistic grass & interactable

--- Assets/Grass/GrassPlanting.cs
+++ Assets/Grass/GrassPlanting.cs
@@ -5,23 +5,25 @@ public class GrassPlanting : MonoBehaviour
 {
     public GameObject grassPrefab;
     private int grassSize = 20;
+    public GameObject interactableObject;
+    public Material material;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         for (int z = -grassSize; z<= grassSize; z++)
         {
             for (int x = -grassSize; x<= grassSize; x++)
-        {
+            {
             Vector3 posistion = new Vector3(x/4.0f + Random.Range(-0.30f,0.30f), 0, z/4.0f + Random.Range(-0.30f,0.30f));
             GameObject grass = Instantiate(grassPrefab, posistion, Quaternion.identity);
-            grass.transform.localScale = new Vector3(1, Random.Range(0.65f, 1.35f), 1);
-        }
+            grass.transform.localScale = new Vector3(1, Random.Range(0.85f, 1.15f), 1);           
+            }
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        material.SetVector("_TramplePosition", interactableObject.transform.position);
     }
 }
